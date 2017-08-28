1f5161a6445ab77aa78fb47deededb13b5792c1c
tanzeelak <tnkhan8042@gmail.com>
2017-08-28 00:48:22 -0700

    parallax home but should fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var slideIndex = 0;
-    showSlides();
+    $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        var target = $(this).attr('href');
+        $('.parallax').animate({
+            scrollTop: $(target).offset().top + $('.parallax').scrollTop()
+        }, 2500);
+        return false;
+    });
+});
 
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        //var dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
-           slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex> slides.length) {slideIndex = 1}
-        // for (i = 0; i < dots.length; i++) {
-        //     dots[i].className = dots[i].className.replace(" active", "");
-        // }
-        slides[slideIndex-1].style.display = "block";
-        //dots[slideIndex-1].className += " active";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+});
 });
